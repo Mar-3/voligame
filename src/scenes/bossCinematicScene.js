57dbb7ad3186942bd51077740419d24ff191b5c8
Mar-3 <96390951+Mar-3@users.noreply.github.com>
2024-10-16 19:15:45 +0300

    Update bossCinematicScene.js

--- src/scenes/bossCinematicScene.js
+++ src/scenes/bossCinematicScene.js
@@ -17,10 +17,10 @@ export default class BossCinematic extends Phaser.Scene {
       { frameWidth: 42, frameHeight: 28});
 
       this.load.spritesheet(BOSS_KEY,
-        '../assets/img/catboss.png',
+        'assets/img/catboss.png',
         { frameWidth: 35, frameHeight:70});
-    this.load.audio('dramatic',['../assets/audio/dramatic.mp3']);
-    this.load.audio('bossattack', ['../assets/audio/boss-attack-1.mp3']);
+    this.load.audio('dramatic',['assets/audio/dramatic.mp3']);
+    this.load.audio('bossattack', ['assets/audio/boss-attack-1.mp3']);
   }
 
   create() {
